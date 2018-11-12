2ab88f000a4777821d83583539e472be290d64c9
Broivula <elias.koivula1@hotmail.com>
2018-11-12 09:51:37 +0200

    Update index.js
    posttest

--- index.js
+++ index.js
@@ -9,5 +9,11 @@ app.get('/', (req, res) => {
   console.log('here we come?');
   res.send('Hello World and some other stuff too, yo');
   console.log('testing nodemon');
-})
+});
+
+app.post('/', (req, res) => {
+  res.send('Hello POST test!!');
+  console.log('POST gets called');
+});
+
 app.listen(3000);
